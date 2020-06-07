fe691070591d0a7984dcb9bfb6fc03923961869e
Adrino Avelar <eam.avelar@gmail.com>
2020-06-07 12:38:35 -0300

    procfile

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 import joblib as jb
 import json
 
-
 app = Flask(__name__)
 
 mdl = jb.load("mdl.pkl.z")
@@ -16,5 +15,9 @@ def main():
     res = {"titulo": title, "p": mdl.predict_proba([title])[0][1]}
     return json.dumps(res)
 
+
+def createapp():
+    return app
+    
 if __name__ == "__main__":
     app.run()
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+from app import createapp
+
+app = createapp()
